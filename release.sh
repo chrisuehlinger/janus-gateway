c303977c20e935e81affc2bdf56fe8a19e077000
Chris Uehlinger <chris.uehlinger@gmail.com>
2020-06-26 00:09:58 -0400

    Why werent we rebooting?

--- release.sh
+++ release.sh
@@ -10,4 +10,4 @@ docker system prune -f
 
 sleep 1
 
-# aws ec2 reboot-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId' --filters "Name=tag:shattered,Values=janus" --output text)
+aws ec2 reboot-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId' --filters "Name=tag:shattered,Values=janus" --output text)
