2a824088a239236cf99daf128998e116ba5b98ac
Chris Uehlinger <chris.uehlinger@gmail.com>
2020-12-03 22:16:21 -0500

    Trying out github actions for builds

--- release.sh
+++ release.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -ex
 
-cat Dockerfile | time docker build -t shattered/janus:x86 -f- . && \
-docker push shattered/janus:x86
+cat Dockerfile | time docker build -t scatteredplayers/janus:x86 -f- . && \
+docker push scatteredplayers/janus:x86
 
 # cat Dockerfile.arm64 | time docker build -t shattered/janus:arm64 -f- . && \
 # docker push shattered/janus:arm64
 
 docker system prune -f
 
-sleep 1
+# sleep 1
 
-aws ec2 reboot-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId' --filters "Name=tag:shattered,Values=janus" --output text)
+# aws ec2 reboot-instances --instance-ids $(aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId' --filters "Name=tag:shattered,Values=janus" --output text)
